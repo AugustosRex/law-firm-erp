b9bf755b252b02cc81c070f807cedc3942c1d35b
AugustosRex <augustosrex@gmail.com>
2026-09-23 20:51:04 +0000

    fix: usa propriedades individuais do banco em vez de parsear DATABASE_URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,29 +3,21 @@ set -e
 
 PORT="${PORT:-10000}"
 
-# Replace port placeholder in Apache configs
 sed -i "s/__PORT__/${PORT}/g" /etc/apache2/ports.conf
 sed -i "s/__PORT__/${PORT}/g" /etc/apache2/sites-available/000-default.conf
 
-# Configure Dolibarr database from Render's DATABASE_URL
-if [ -n "$DATABASE_URL" ]; then
-    DB_USER=$(echo "$DATABASE_URL" | sed -n 's|.*://\([^:]*\):.*|\1|p')
-    DB_PASS=$(echo "$DATABASE_URL" | sed -n 's|.*://[^:]*:\([^@]*\)@.*|\1|p')
-    DB_HOST=$(echo "$DATABASE_URL" | sed -n 's|.*@\([^:]*\):.*|\1|p')
-    DB_PORT=$(echo "$DATABASE_URL" | sed -n 's|.*:\([0-9]*\)/.*|\1|p')
-    DB_NAME=$(echo "$DATABASE_URL" | sed -n 's|.*/\([^/?]*\).*|\1|p')
-
+if [ -n "$DOLIBARR_DB_HOST" ]; then
     cat > /var/www/html/dolibarr/htdocs/conf/conf.php << EOF
 <?php
 \$dolibarr_main_url_root='${DOLIBARR_URL_ROOT:-http://localhost}';
 \$dolibarr_main_document_root='/var/www/html/dolibarr/htdocs';
 \$dolibarr_main_data_root='/var/www/html/dolibarr/documents';
 \$dolibarr_main_db_type='pgsql';
-\$dolibarr_main_db_host='$DB_HOST';
-\$dolibarr_main_db_port='$DB_PORT';
-\$dolibarr_main_db_name='$DB_NAME';
-\$dolibarr_main_db_user='$DB_USER';
-\$dolibarr_main_db_pass='$DB_PASS';
+\$dolibarr_main_db_host='$DOLIBARR_DB_HOST';
+\$dolibarr_main_db_port='$DOLIBARR_DB_PORT';
+\$dolibarr_main_db_name='$DOLIBARR_DB_NAME';
+\$dolibarr_main_db_user='$DOLIBARR_DB_USER';
+\$dolibarr_main_db_pass='$DOLIBARR_DB_PASS';
 \$dolibarr_main_db_prefix='llx_';
 \$dolibarr_main_db_character_set='UTF8';
 \$dolibarr_main_db_collation='';
